cc7549f1da392eb7a4e5ff4f7bde7391ad20ee02
louie <727057301@qq.com>
2020-06-26 19:28:58 +0800

    [ADD] 增加以太坊交易记录、账户地址余额的接口

--- src/django/src/blockchain/views.py
+++ src/django/src/blockchain/views.py
@@ -1,30 +1,82 @@
 
 from rest_framework.views import APIView
 from utils.json_response import JsonResponse
-from blockchain.coins.bitcoin import Bitcoin
+from blockchain.coins.bitcoin import BitcoinExplorer,esplora
+from blockchain.coins.ethereum import ETHExplorer
 
 
 
-class AddressView(APIView):
+class BTCAddressView(APIView):
     def get(self, request, address, format=None):
         real_time = request.GET.get('real-time')
-        b = Bitcoin(address)
-        data = b.info(real_time)
+
+        b = BitcoinExplorer(address)
+
+        try:
+            data = b.info(real_time)
+        except Exception:
+            return JsonResponse({}, 500, "Server cannot find, Please try again")
+
         return JsonResponse(data, 0, "success")
+
+
+class ETHAddressView(APIView):
+    def get(self, request, address, format=None):
+        real_time = request.GET.get('real-time')
+
+        b = ETHExplorer()
+
+        try:
+            data = b.call('address',address)
+        except Exception:
+            return JsonResponse({}, 500, "Server cannot find, Please try again")
+
+        return JsonResponse(data, 0, "success")
+
 
 
 class AddressUTXOView(APIView):
     def get(self, request, address, format=None):
         real_time = request.GET.get('real-time')
-        b = Bitcoin(address)
-        data = b.utxo(real_time)
+        b = BitcoinExplorer(address)
+
+        try:
+            data = b.utxo(real_time)
+        except Exception:
+            return JsonResponse({}, 500, "Server cannot find, Please try again")
+
         return JsonResponse(data, 0, "success")
+
+
+class ETHAddressTransactionView(APIView):
+    def get(self, request, address, format=None):
+        real_time = request.GET.get('real-time')
+        page = request.GET.get('page',1)
+        page_size = request.GET.get('size',25)
+        e = ETHExplorer()
+
+        try:
+            data = e.call('txs', address, page, page_size)
+        except Exception as e:
+            print(e)
+            return JsonResponse({}, 500, "Server cannot find, Please try again")
+
+        return JsonResponse(data, 0, "success")
+
 
 
 
 class AddressTransactionView(APIView):
     def get(self, request, address, format=None):
         real_time = request.GET.get('real-time')
-        b = Bitcoin(address)
-        data = b.transaction(real_time)
+        e = esplora()
+
+        # try:
+        data = e.call('txs',address)
+        # except Exception:
+        #     return JsonResponse({}, 500, "Server cannot find, Please try again")
+
         return JsonResponse(data, 0, "success")
+
+
+
