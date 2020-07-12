35574ed7398e1f1ba4f285e36b4a888d2eecabde
louie <727057301@qq.com>
2020-07-13 00:20:05 +0800

    fix `No module named 'django.core.context_processors' bugs` caused by translate module

--- src/otcserver/settings.py
+++ src/otcserver/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 # translate module
-                "django.core.context_processors.i18n",
+                "django.template.context_processors.i18n",
             ],
         },
     },
