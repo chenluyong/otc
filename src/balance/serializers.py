1b731167cf296b7779347bd4a9cc3f43a2a4131c
louie <727057301@qq.com>
2020-07-13 13:08:54 +0800

    stop balance views dev

--- src/balance/serializers.py
+++ src/balance/serializers.py
@@ -1,16 +1,20 @@
-from rest_framework import serializers
-from balance.models import History as BalanceHistoryModel
+# from rest_framework import serializers
+# from balance.models import History as BalanceHistoryModel
+#
+#
+# class BalanceHistorySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = BalanceHistoryModel
+#
+#         fields = '__all__'
+#
+#
+# class BalanceDetailSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = BalanceHistoryModel
+#
+#         fields = '__all__'
 
 
-class BalanceHistorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BalanceHistoryModel
 
-        fields = '__all__'
-
-
-class BalanceDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BalanceHistoryModel
-
-        fields = '__all__'
+########################################
--- src/balance/urls.py
+++ src/balance/urls.py
@@ -6,15 +6,15 @@ from balance import views
 from . import rpc
 
 urlpatterns = [
-    # 修改资产：充值、扣款
-
-    # 获取概要交易记录
-    url(r'^history/$', views.BalanceHistoryListView.as_view(),name='balance_history'),
-    # 通过交易记录查订单详情
-    url(r'^history/(?P<history_id>(\d{1,32}))$', views.BalanceHistoryDetailView.as_view(),name='balance_history_detail'),
-
-    # 获取特定币种余额
-    url(r'^(?P<coin_name>([a-zA-Z]{3,4}))$', views.BalanceDetailView.as_view(),name='balance'),
+    # # 修改资产：充值、扣款
+    #
+    # # 获取概要交易记录
+    # url(r'^history/$', views.BalanceHistoryListView.as_view(),name='balance_history'),
+    # # 通过交易记录查订单详情
+    # url(r'^history/(?P<history_id>(\d{1,32}))$', views.BalanceHistoryDetailView.as_view(),name='balance_history_detail'),
+    #
+    # # 获取特定币种余额
+    # url(r'^(?P<coin_name>([a-zA-Z]{3,4}))$', views.BalanceDetailView.as_view(),name='balance'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
